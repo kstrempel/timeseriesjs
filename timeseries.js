93c5ed0f43e41ea8ec53fd9407dabe3d4a407045
Kai Strempel <kstrempel@gmail.com>
2014-02-17 17:28:55 +0100

    first commit

---
+++ timeseries.js
@@ -0,0 +1,99 @@
+
+var _ = require('highland'),
+    fs = require('fs'),
+    async = require('async'),
+    numeral = require('numeral'),
+    util = require('util');
+
+var start = new Date();
+
+function toLines() {
+
+    return function(err, x, push, next){
+        if (x === _.nil) {
+            push(null, _.nil);
+        }
+        else {
+            _(x.split("\n")).each(function(line){
+                var parts = line.split("\t");
+                if(parts.length==2)
+                    push(null, [parts[0], parseFloat(parts[1])]);
+            });
+
+            next();
+        }
+    }
+}
+
+
+function toGroups(){
+
+    var grouping = { time : 0, values : []};
+
+    return function (err, x, push, next) {
+        if (x === _.nil) {
+            push(null,grouping);
+            push(null, _.nil);
+            console.log( (new Date()).getTime() - start.getTime() );
+        }
+        else {
+            var time = x[0];
+            var value = x[1];
+
+            if(grouping.time == time){
+                grouping.values.push(value);
+            }else{
+                push(null,grouping);
+                grouping = { time : time, values : [value] };
+            }
+
+            next();
+        }
+    }
+}
+
+function getStream(fileToLoad){
+    var stream = fs.createReadStream(fileToLoad, {
+            flags : 'r',
+            encoding : 'utf-8',
+            fd : null
+        });
+
+    return _(stream).consume(toLines()).consume(toGroups());
+}
+
+function main(){
+
+    if(process.argv.length != 3){
+        console.log("usage <filename to import>")
+        process.exit(0);
+    }
+    var fileToLoad = process.argv[2];
+
+    var iter = getStream(fileToLoad);
+    console.log("Time       Value  N_O Roll_Sum Min_Value Max_Value");
+    console.log("---------------------------------------------------");
+
+    iter.each(function(data){
+        async.parallel({
+            sm : function(callback){
+                _(data.values).reduce(0, function(a,b){return a+b;}).toArray(function(xs){callback(null,xs)})
+            },
+            min : function(callback){
+                _(data.values).reduce1(function(a,b){return a<b ? a : b;}).toArray(function(xs){callback(null,xs)})
+            },
+            max : function(callback){
+                _(data.values).reduce1(function(a,b){return a>b ? a : b;}).toArray(function(xs){callback(null,xs)})
+            }
+        },function(err,results){
+            console.log(util.format("%s %s %s  %s  %s   %s",data.time,
+                                                            data.values[0],
+                                                            data.values.length,
+                                                            numeral(parseFloat(results.sm)).format("0[.]00000"),
+                                                            results.min,
+                                                            results.max));
+        });
+    });
+}
+
+main();
